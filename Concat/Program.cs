d2a6dd5ca47cc67f1def2c2a87c58acd0db2796b
Anton <anton.sakovich@softteco.com>
2022-04-20 20:09:16 +0300

    [Concat] Add example

---
+++ Concat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Concat
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss.ffff") + ".log";
+            string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "/", fileName);
+
+            string concatenatedArgs = String.Concat(args);
+
+            File.WriteAllText(filePath, concatenatedArgs + Environment.NewLine);
+            Console.WriteLine(concatenatedArgs);
+        }
+    }
+}
