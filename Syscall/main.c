a973b748a1cf3e62278ebc7e86b2311c579d5418
Anton <anton.sakovich@softteco.com>
2022-04-21 16:40:16 +0300

    [Syscall] Update returned error codes

--- Syscall/main.c
+++ Syscall/main.c
@@ -8,8 +8,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #define STACK_SIZE (1024 * 1024)
+
+#define APPERR_MMAP_FAILED 201
+#define APPERR_WAIT_FAILED 202
 
 static int child_main(void *arg)
 {
@@ -22,7 +26,7 @@ int main(int argc, char **argv)
     char *pFirstStackByte = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
     if (pFirstStackByte == MAP_FAILED)
     {
-        return 1;
+        return APPERR_MMAP_FAILED;
     }
 
     char *pLastStackByte = pFirstStackByte + STACK_SIZE;
@@ -30,7 +34,7 @@ int main(int argc, char **argv)
     pid_t pid = clone(child_main, pLastStackByte,  CLONE_NEWPID | SIGCHLD, NULL);
     if (pid == -1)
     {
-        return 2;
+        return errno;
     }
 
     printf("Child PID as seen from host: %i\n", pid);
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
     pid = waitpid(pid, NULL, 0);
     if (pid == -1)
     {
-        return 3;
+        return APPERR_WAIT_FAILED;
     }
 
     exit(EXIT_SUCCESS);
